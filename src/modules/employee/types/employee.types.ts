dcdc9ffac103d54accaf8278a202923011a11ebb
Ankita <Ankitajoshi123>
2026-03-18 17:35:28 +0530

    dashboard

--- src/modules/employee/types/employee.types.ts
+++ src/modules/employee/types/employee.types.ts
@@ -1,7 +1,25 @@
-export interface Visit {
-  id?: number;
-  visitDate: string;
-  companyName: string;
+// src/types.ts
+
+// Visit Tracking Types
+export type Schedule = {
+  place: string;
+  task: string;
+  time: string;
+};
+
+export type Expense = {
+  type: string;
+  amount: string;
+};
+
+export type Visit = {
+  company: string;
   purpose: string;
-  expense?: number;
-}
+  location: string;
+  date: string;
+  followUp: string;
+  stage: "Lead" | "Discussion" | "Proposal" | "Negotiation" | "Won" | "Lost";
+  outcome: string;
+};
+
+export type TabType = "visit" | "schedule" | "expense";
